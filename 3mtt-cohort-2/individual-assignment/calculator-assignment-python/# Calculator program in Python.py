6293828ead3b5e0f9d8136dc400406235bec7e9d
umardeebaros <umardeebaros@gmail.com>
2024-09-26 12:48:02 +0100

    FE/23/40952000

---
+++ 3mtt-cohort-2/individual-assignment/calculator-assignment-python/# Calculator program in Python.py
@@ -0,0 +1,56 @@
+# Calculator program in Python
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error! Division by zero."
+
+def percentage(x, y):
+    return (x * y) / 100
+
+# Main function to operate the calculator
+def calculator():
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Percentage")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice in ['1', '2', '3', '4', '5']:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(f"{num1} + {num2} = {add(num1, num2)}")
+
+        elif choice == '2':
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+
+        elif choice == '3':
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+
+        elif choice == '4':
+            result = divide(num1, num2)
+            print(f"{num1} / {num2} = {result}")
+
+        elif choice == '5':
+            print(f"{num1}% of {num2} = {percentage(num1, num2)}")
+    else:
+        print("Invalid Input")
+
+if __name__ == "__main__":
+    
+    calculator()
